c86eaea76d180a557c622eb0c7c910611e0b8a9b
luckyowl11 <sudhanshuseenver3.0@gmail.com>
2023-08-20 16:19:18 +0530

    as

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// ///////////////////////////////////
 const express = require("express");
 const axios = require("axios");
 
@@ -7,18 +8,39 @@ app.use(express.json());
 
 async function token(apiKey, res) {
   const config = { headers: { "Api-key": apiKey } };
-  const token = await axios.post(
-    "https://dev-test.cimet.io/generate-token",
-    config
-  );
-  return res.json(token.data);
+
+  try {
+    const resp = await axios.post(
+      "https://dev-test.cimet.io/generate-token",
+      null, // No data needs to be sent in the request body
+      config
+    );
+    const data = resp.data; // No need to wrap resp.data in res.json
+
+    console.log("token running", data);
+    res.json(data); // Use res.json directly here
+  } catch (error) {
+    console.error("Error generating token:", error);
+    res.status(500).json({ error: "Internal server error" }); // Handle error response
+  }
 }
 
-app.post("/token", (req, res) => {
-  const { headers } = req;
-  token(headers["Api-key"], res);
-  // res.send('Hello World!');
+app.get("/get", (req, res) => {
+  console.log("req--", req);
+  res.send(200).json("hello");
 });
+
+app.post("/token", (req, res) => {
+  console.log("request running");
+  const { headers } = req;
+  console.log("headers", headers);
+  //   res.send(200);
+  token(headers["api-key"], res); // Use lowercase "api-key"
+});
+
+// let fetchList(())
+
+// app.post("/list", (req, res) => {});
 
 app.listen(3000, () => {
   console.log("Express server initialized");
