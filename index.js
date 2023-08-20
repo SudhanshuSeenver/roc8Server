5bfb5edfade442d51639fcc1107c55becc7b26c9
luckyowl11 <sudhanshuseenver3.0@gmail.com>
2023-08-20 15:46:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+
+async function token(apiKey, res) {
+  const config = { headers: { "Api-key": apiKey } };
+  const token = await axios.post(
+    "https://dev-test.cimet.io/generate-token",
+    config
+  );
+  return res.json(token.data);
+}
+
+app.post("/token", (req, res) => {
+  const { headers } = req;
+  token(headers["Api-key"], res);
+  // res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log("Express server initialized");
+});
